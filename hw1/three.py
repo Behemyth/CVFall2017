6f62af20ae8915075be7f4a04fc29cd2d33884c7
uyenuong <uyen.uong33@gmail.com>
2017-09-22 04:31:58 -0400

    completed implementation of seam carving and tried it on another twinning movie

--- hw1/three.py
+++ hw1/three.py
@@ -9,21 +9,31 @@ import random
 import sys
 import math
 
+'''
+This program takes in 3 arguments: the name of the source video,
+the name of the target video, and the name of the output video.
+
+It uses codec MP4V so the output video MUST but .mp4 in order
+for the video to output correctly
+'''
+
 # Runs through all of the frames in the video
-# Returns image that is an average of all the frames
+# Returns image that is the average of all the frames
 def FlattenImage(vid) :
 	# Holds the sum of all the frames' RGB values at each pixel
-	sumImg = np.zeros( (int(vid.get(4)), int(vid.get(3)), 3) )
+	sumImg = np.zeros( (int(vid.get(4)), int(vid.get(3)), 3), dtype=np.float )
 
-	# Loop through all of the frames or either source or target and add to sum
+	# Loop through all of the frames add to sum
 	while(True):
 		ret, frame = vid.read()
 		if (not ret):
 			# Reached end of file
 			break
+		
+		frame = frame.astype(np.float)
 		sumImg += frame
 
-	return sumImg / vid.get(3)
+	return sumImg / vid.get(7) # property 7 is the number of frames
 
 # Determines the best seam to composite two images together
 # Source will be image on the left of the seam, target will be image on the right
@@ -54,6 +64,10 @@ def FindSeam(source, target):
 					# Only need to look at 2 pixels above
 					currentEnergy = np.linalg.norm(source[y][x])
 					energy[y][x] = min( (energy[y-1][x] + currentEnergy), (energy[y-1][x-1] + currentEnergy) )
+				elif (x == 0):
+					# Only need to look at 2 pixels above
+					currentEnergy = np.linalg.norm(source[y][x])
+					energy[y][x] = min( (energy[y-1][x] + currentEnergy), (energy[y-1][x+1] + currentEnergy) )
 				else:
 					# Need to look at the 3 pixels above because that is how got to current pixel
 					currentEnergy = np.linalg.norm(np.absolute(target[y][x+1] - source[y][x]))
@@ -73,19 +87,33 @@ def CreateMask(energy):
 			index = x
 
 	# Backtracing along the seam and creating a mask from it
-	mask = np.zeros( (energy.shape) )
+	mask = np.zeros( (energy.shape), dtype=np.float )
 	mask[-1][:index+1] = 255  # making the whole row up to and including the index a 1
-	print(energy.shape[0])
 	for row in range(energy.shape[0]-1,0,-1):
 		# At each row, look at the row above and of the 3 possible pixels that the min cost path could've 
 		# traversed, take the index of the pixel with the smallest energy cost
 		# Make the whole row up to and including the index 1
-		minCost = min( min( energy[row-1][index-1], energy[row-1][index+1] ), energy[row-1][index] )
 
-		if (energy[row-1][index-1] == minCost):
-			index = index - 1
-		elif (energy[row-1][index+1] == minCost):
-			index = index + 1
+		# Checking edge cases
+		if (index == 0):
+			minCost = min( energy[row-1][index], energy[row-1][index+1] )
+
+			if (energy[row-1][index+1] == minCost):
+				index = index + 1
+
+		elif (index == energy.shape[1]-1):
+			minCost = min( energy[row-1][index], energy[row-1][index-1] )
+
+			if (energy[row-1][index+1] == minCost):
+				index = index + 1
+
+		else:
+			minCost = min( min( energy[row-1][index-1], energy[row-1][index+1] ), energy[row-1][index] )
+
+			if (energy[row-1][index-1] == minCost):
+				index = index - 1
+			elif (energy[row-1][index+1] == minCost):
+				index = index + 1
 
 		mask[row-1][:index+1] = 255
 
@@ -98,14 +126,13 @@ def CreateMask(energy):
 # Composites the source on the left and the target on the right and creates the video file
 def CreateComposite(source, target, mask, outputName):
 	compositeVideo = cv2.VideoWriter(outputName, 
-									cv2.VideoWriter_fourcc('h','2','6','4'),  # using H.264 codec
+									cv2.VideoWriter_fourcc('m','p','4','v'), 
 									source.get(5),  # setting the frame rate of composite to be same as source
-									(int(source.get(3)), int(source.get(4))))  # setting size of composite to be same as source
+									(int(source.get(3)), int(source.get(4))), True)  # setting size of composite to be same as source
 	
 	# Need to convert the values in the mask from grayscale to 0-1
-	print(mask)
-	mask = (255.0 - mask) / 255.0
-	print(mask)
+	mask = mask / 255.0
+	mask = mask[:,:,np.newaxis] # Comment out when reading in mask file
 
 	# Loop through frames of source and target and
 	# composite them together until no more frames in one
@@ -119,9 +146,7 @@ def CreateComposite(source, target, mask, outputName):
 
 		# Composite source and target using linear interpolation
 		compositeImage = mask * frameSource + (1-mask) * frameTarget
-		print(compositeImage.shape)
-		print(frameSource.shape)
-		# print(compositeImage)
+		compositeImage = compositeImage.astype('uint8')  # Need to convert type or else it will not be able to write!
 
 		# Add this frame to the composite video
 		compositeVideo.write(compositeImage)
@@ -150,22 +175,21 @@ if __name__ == "__main__":
 		sys.exit()
 
 	# Makes sure that the videos are the same size
+	# 3rd property of videos is the width
+	# 4th property of videos is the height
 	if (source.get(3) != target.get(3) and \
 		source.get(4) != target.get(4)):
 		print('Videos are not the same size')
 		sys.exit()
 
-	# Get video size and create an empty image of the same size
-	width = int(source.get(3))  # 3rd property of videos is the width
-	height = int(source.get(4)) # 4th property of videos is the height
-	combinedAvg = np.zeros((height, width, 3))  # holds the average of all frames
-
 	# Get the average frame of each video 
-	# avgSource = FlattenImage(source)
-	# avgTarget = FlattenImage(target)
-	avgSource = cv2.imread('avgSource.jpg')
-	avgTarget = cv2.imread('avgTarget.jpg')
+	avgSource = FlattenImage(source)
+	avgTarget = FlattenImage(target)
 
+	
+	# FOR DEBUGGING
+	# avgSource = cv2.imread('avgSource.jpg')
+	# avgTarget = cv2.imread('avgTarget.jpg')
 	# cv2.imwrite('avgSource.jpg', avgSource)
 	# cv2.imwrite('avgTarget.jpg', avgTarget)
 
@@ -173,9 +197,10 @@ if __name__ == "__main__":
 	source.open(sourceName)
 	target.open(targetName)
 
-	# mask = CreateMask(FindSeam(avgSource, avgTarget))
-	mask = cv2.imread('twin_mask.jpg')
+	mask = CreateMask(FindSeam(avgSource, avgTarget))
 
+	# FOR DEBUGGING
+	# mask = cv2.imread('twin_mask_asher12.jpg')
 	# cv2.imwrite('twin_mask.jpg', mask)
 
 	CreateComposite(source, target, mask, outputName)
