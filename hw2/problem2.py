d1e907ed4e853e0027f08c1bab3acf4cdf38d907
Behemyth <asher.norland@gmail.com>
2017-10-02 20:39:24 -0400

    Added initial kivy

--- hw2/problem2.py
+++ hw2/problem2.py
@@ -1,1 +1,27 @@
 
+import cv2         
+import numpy as np
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.graphics.texture import Texture
+
+# imports
+import os
+import random
+import sys
+import math
+
+if __name__ == "__main__":
+    
+    imgName = sys.argv[1]
+    img = cv2.imread(imgName)
+    
+    texture = Texture.create(size=(16, 16), colorfmt="rgb")
+    
+    arr = np.ndarray(shape=[16, 16, 3], dtype=np.uint8)
+
+
+    data = arr.tostring()
+    texture.blit_buffer(data, bufferfmt="ubyte", colorfmt="rgb")
