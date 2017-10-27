6b10e6f932c44171efb7cbc186a05db36506beb2
Behemyth <asher.norland@gmail.com>
2017-10-27 04:23:49 -0400

    Remove frame

--- hw3/problem2.py
+++ hw3/problem2.py
@@ -29,19 +29,27 @@ def DiffFrames(frame1, frame2):
 
 # Cuts each frame of the video along the seam in the mask
 # Composites the source on the left and the target on the right and creates the video file
-def CreateComposite(source,frame, outputName):
+def CreateComposite(source, outputName):
     compositeVideo = cv2.VideoWriter(outputName, cv2.VideoWriter_fourcc('D','I','V','X'), source.get(5), (int(source.get(3)), int(source.get(4))), True)  
 	
-    frameTest = GetCorners(frame)
+    prevFrame = None
 
 	# Loop through frames of source and target and
 	# composite them together until no more frames in one
     while(True):
         retSource, frameSource = source.read()
  
+        #grab the corners of this image
         pixelTest = GetCorners(frameSource)
 
-        difference = DiffFrames(pixelTest, frameTest)
+        #set if none
+        if prevFrame is None:
+            prevFrame = pixelTest
+
+        #find the related points between each frame
+        closestMatches = DiffFrames(pixelTest, prevFrame)
+
+        #calculate the homography
 
         cv2.imshow('Harris Corner Detector',pixelTest.astype(np.uint8)*255)
         if cv2.waitKey(0) & 0xff == 27:
@@ -64,7 +72,6 @@ if __name__ == "__main__":
 
     sourceName = sys.argv[1]
     outputName = sys.argv[2]
-    frameName = sys.argv[3]
 
     # Open the videos
     source = cv2.VideoCapture(sourceName)
@@ -74,8 +81,6 @@ if __name__ == "__main__":
         print('Videos did not open correctly')
         sys.exit()
 
-    frame = cv2.imread(frameName)
-
-    CreateComposite(source,frame, outputName)
+    CreateComposite(source, outputName)
 
 
