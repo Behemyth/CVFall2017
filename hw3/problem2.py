7d8e37d96d90bcd6d01928fc3d497cef25103b25
Behemyth <asher.norland@gmail.com>
2017-10-27 04:07:59 -0400

    Edit

--- hw3/problem2.py
+++ hw3/problem2.py
@@ -14,40 +14,37 @@ import math
 
 neighborhood = 5
 
+def GetCorners(image):
+    #find the features for the frame image
+    #read the frame as grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(gray,30,255,0)
+
+    #grab the harris corner points
+    corners = cv2.cornerHarris(thresh,4,1,0.04)
+ 
+    return corners>0.001
+
 def DiffFrames(frame1, frame2):
-    return np.square(frame2 - frame1).sum().T
+    return np.square((frame2 - frame1)**2).sum()
 
 
 # Cuts each frame of the video along the seam in the mask
 # Composites the source on the left and the target on the right and creates the video file
-def CreateComposite(source, outputName):
+def CreateComposite(source,frame, outputName):
     compositeVideo = cv2.VideoWriter(outputName, cv2.VideoWriter_fourcc('D','I','V','X'), source.get(5), (int(source.get(3)), int(source.get(4))), True)  
 	
-
-    prevFrame = None
+    frameTest = GetCorners(frame)
 
 	# Loop through frames of source and target and
 	# composite them together until no more frames in one
     while(True):
         retSource, frameSource = source.read()
-
-        original = np.copy(frameSource)
-
-        #read the frame as grayscale
-        graySource = cv2.cvtColor(frameSource, cv2.COLOR_BGR2GRAY)
-        ret,thresh = cv2.threshold(graySource,30,255,0)
-
-        #grab the harris corner points
-        corners = cv2.cornerHarris(thresh,4,1,0.04)
  
-        pixelTest = corners>0.001
+        pixelTest = GetCorners(frameSource)
 
-        if prevFrame is None:
-            prevFrame = pixelTest
+        DiffFrames(pixelTest, frameTest)
 
-        DiffFrames(pixelTest, prevFrame)
-
-        prevFrame = pixelTest
 
         cv2.imshow('Harris Corner Detector',pixelTest.astype(np.uint8)*255)
         if cv2.waitKey(0) & 0xff == 27:
@@ -64,7 +61,7 @@ def CreateComposite(source, outputName):
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) != 3) :
+    if (len(sys.argv) != 4) :
         print('Invalid number of arguments')
         sys.exit()
 
@@ -80,6 +77,8 @@ if __name__ == "__main__":
         print('Videos did not open correctly')
         sys.exit()
 
-    CreateComposite(source, outputName)
+    frame = cv2.imread(frameName)
+
+    CreateComposite(source,frame, outputName)
 
 
