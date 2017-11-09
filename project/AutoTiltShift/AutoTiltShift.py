6709db643f9914a71d8d6563d31e7573d8c0d8aa
Behemyth <asher.norland@gmail.com>
2017-11-09 17:11:25 -0500

    Initial Commit

--- project/AutoTiltShift/AutoTiltShift.py
+++ project/AutoTiltShift/AutoTiltShift.py
@@ -1,1 +1,15 @@
+import cv2         
+import numpy as np
+from matplotlib import pyplot as plt 
+import matplotlib   
+
+# imports
+import os
+import random
+import sys
+
+if __name__ == "__main__":
+
+       videoName = sys.argv[1]
+
 
